3feb82065a2a4b21f0da0a14f2524f28de882311
Edwin Bosire <me@edwinb.co.uk>
2020-03-15 18:11:58 +0000

    Implemented the instructions for moving a robot according to the specs

--- MarsRover/Robot.swift
+++ MarsRover/Robot.swift
@@ -56,15 +56,55 @@ class Robot {
         return true
     }
     
+    /// Turns the robot's orientation 90º Left.
+    /// The following rules apply (=> implies changes to)
+    ///
+    /// north => west
+    /// west => south
+    /// south => east
+    /// east => north
     private func turnLeft() {
-        
+        switch orientation {
+        case .north: orientation = .west
+        case .west: orientation = .south
+        case .south: orientation = . east
+        case .east: orientation = .north
+        }
     }
     
+    /// Turns the robot's orientation 90º Right.
+    /// The following rules apply (=> implies changes to)
+    ///
+    /// north => east
+    /// west => north
+    /// south => west
+    /// east => south
     private func turnRight() {
-        
+        switch orientation {
+        case .north: orientation = .east
+        case .west: orientation = .north
+        case .south: orientation = . west
+        case .east: orientation = .south
+        }
     }
     
+    /// Reads current orientation and moves one step in that direction
+    /// The following rules apply
+    ///
+    /// if orientation == north: (x, y+1)
+    /// if orientation == south: (x, y-1)
+    /// if orientation == east: (x+1, y)
+    /// if orientation == west: (x-1, y)
+
     private func moveForward() {
+        var newLocation = location
         
+        switch orientation {
+        case .north: newLocation.y += 1
+        case .east: newLocation.y -= 1
+        case .west: newLocation.x -= 1
+        case .south: newLocation.x += 1
+        }
+        location = newLocation
     }
 }
